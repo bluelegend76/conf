4a35308c3a6d318a4a9993efca417edcc0e848b7
bluelegend <trulystrange@gmail.com>
2026-02-14 03:19:57 +0100

    Starts adding central Nix-flake and Home Manager

---
+++ nix-fleet/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Empire Fleet - Step 1";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.high-end = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        # Point to your existing config
+        /etc/nixos/configuration.nix
+
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.bluelegend = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
