71ecedbc09c72120606223c522a4adab9205d78f
bluelegend <trulystrange@gmail.com>
2026-02-19 20:57:31 +0100

    Tries out Emacs PGTK-version

--- nix-fleet/modules/editors/emacs.nix
+++ nix-fleet/modules/editors/emacs.nix
@@ -1,115 +1,79 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, config, ... }:
 
 let
-  # Detect presence of ARM-based mobile-device
   isMobile = pkgs.stdenv.hostPlatform.isAarch64;
-  # The "Universal Path" logic
   syncPath = if isMobile
              then "/storage/emulated/0/SyncThing"
              else "${config.home.homeDirectory}/SyncThing";
 
-  # Merge your specific performance overrides here
-  myEmacs = (pkgs.emacs30-pgtk.override {
+  myEmacs = (pkgs.emacs30.override {
     withTreeSitter = true;
     withNativeCompilation = true;
   });
 in {
-  # This starts the Emacs server in the background
-  # services.emacs = {
   programs.emacs = {
     enable = true;
     package = myEmacs;
 
     extraPackages = epkgs: with epkgs; [
-      # The Vim Expat Trifecta
-      evil
-      evil-collection
-      which-key
-      general
-
-      # Core Navigation & Aesthetics
-      ivy
-      counsel
-      doom-themes
-      doom-modeline
-      magit
-
-      # "Smart Syntax" (Highlighting only; No binaries)
-      markdown-mode
-      # lilypond
-      clojure-mode
-      cider
-      lua-mode
-      nix-mode
-
+      evil evil-collection which-key general
+      ivy counsel doom-themes doom-modeline magit
+      markdown-mode clojure-mode cider lua-mode nix-mode
       treesit-grammars.with-all-grammars
     ];
 
     extraConfig = ''
-      ;; --- 1. CORE UI ---
+      ;; 1. THE TOP-LEVEL LOCKS (Must be first)
+      (setq inhibit-startup-screen t)
       (setq inhibit-startup-message t)
-      (setq inhibit-splash-screen t)
-      (setq initial-scratch-message nil) ; or ""
+      (setq inhibit-flash-screen t)
+      (setq initial-scratch-message nil)
+      (setq initial-major-mode 'fundamental-mode)
+      (setq inhibit-startup-echo-area-message "bluelegend")
 
-      (setq initial-major-mode 'fundamental-mode) ; For a quick startup
-      (scroll-bar-mode -1)
-      (tool-bar-mode -1)
-      (blink-cursor-mode 0)
-
-      ;; --- 2. VIM EXPAT TRIFECTA ---
+      ;; 2. THE ENGINE
       (setq evil-want-keybinding nil)
       (require 'evil)
       (evil-mode 1)
-
       (require 'evil-collection)
       (evil-collection-init)
-
       (require 'which-key)
       (which-key-mode)
 
-      ;; --- 3. THEME & CYCLER ---
+      ;; 3. THEME & KEYBINDINGS
       (require 'doom-themes)
       (load-theme 'doom-ir-black t)
 
-      (defvar my/themes '(doom-ir-black doom-one doom-vibrant doom-molokai))
-      (defun my/cycle-theme ()
-        (interactive)
-        (let* ((current (car custom-enabled-themes))
-               (next (or (cadr (member current my/themes)) (car my/themes))))
-          (mapc #'disable-theme custom-enabled-themes)
-          (load-theme next t)
-          (message "Switched to theme: %s" next)))
-
-      ;; --- 4. GENERAL KEYBINDINGS (Leader: ,) ---
       (require 'general)
-      (general-create-definer my-leader-def
-        :keymaps '(normal insert visual emacs)
+      (general-define-key
+        :states '(normal insert visual emacs)
         :prefix ","
-        :global-prefix "C-,")
-      
-      (my-leader-def
+        :global-prefix "C-,"
         "f"  '(:ignore t :which-key "files")
-        "ff" '(find-file :which-key "find file")
-        "bb" '(switch-to-buffer :which-key "switch buffer")
-        "gs" '(magit-status :which-key "git status")
+        "ff" 'find-file
+        "bb" 'switch-to-buffer
+        "gs" 'magit-status
         "t"  '(:ignore t :which-key "themes")
-        "tn" '(my/cycle-theme :which-key "next theme")
+        "tn" (lambda () (interactive) 
+               (let* ((current (car custom-enabled-themes))
+                      (next (if (eq current 'doom-ir-black) 'doom-one 'doom-ir-black)))
+                 (mapc #'disable-theme custom-enabled-themes)
+                 (load-theme next t)))
         "s"  '(:ignore t :which-key "sync")
-        "sd" `(lambda () (interactive) (dired "${syncPath}")))
+        "sd" (lambda () (interactive) (dired "${syncPath}")))
+
+      ;; 4. THE DAEMON FIX (The only hook we need)
+      (add-hook 'focus-in-hook (lambda () (load-theme 'doom-ir-black t)))
     '';
   };
 
-  # External helper binaries
-  home.packages = with pkgs; [
-    ripgrep
-    fd
-  ];
-
-  # xdg.configFile."emacs/init.el".source = ./init.el;
-
+  # Make sure the service is NOT fighting for the same binary
   services.emacs = {
     enable = true;
-    # Use the same high-perf package for the deamon
     package = myEmacs;
+  };
+
+  home.shellAliases = {
+    emacs = "emacsclient -c -a ''";
   };
 }
